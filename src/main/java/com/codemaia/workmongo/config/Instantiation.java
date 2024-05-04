d92a6c42d32abb6c81688e7c07f9372e6859dd47
codemaia <renanmaia.rl@gmail.com>
2024-05-03 22:10:08 -0300

    instantiation database

---
+++ src/main/java/com/codemaia/workmongo/config/Instantiation.java
@@ -0,0 +1,35 @@
+package com.codemaia.workmongo.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.codemaia.workmongo.domain.Book;
+import com.codemaia.workmongo.repository.BookRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner{
+
+	@Autowired // Dependency Injection
+	private BookRepository bookRepository;	
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub	
+		
+		bookRepository.deleteAll(); //delete all book in mongodb
+		
+		Book book1 = new Book(null, "Engenharia de Software", "Somerville", "Futuro");
+		Book book2 = new Book(null, "Estruturade dados e algoritmos com JavaScript", "Loiane Groner", "Novatec");
+		Book book3 = new Book(null, "Entendendo Algoritmos: Um Guia Ilustrado Para Programadores e Outros Curiosos", " Aditya Y. Bhargava", "Novatec");
+		
+		
+		bookRepository.saveAll(Arrays.asList(book1, book2, book3));
+		
+		
+	}
+	
+
+}
